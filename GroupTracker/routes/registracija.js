e9aa4954d306f8f63b2752e877c95e5cf198010c
shaax <shaaxishhh@gmail.com>
2015-06-02 22:16:00 +0200

    reg dodatek
    uporabnik ob registraciji dobi default skupino

--- GroupTracker/routes/registracija.js
+++ GroupTracker/routes/registracija.js
@@ -15,23 +15,44 @@ var Uporabnik = bookshelf.Model.extend({
 
 
 });
+var SkupinaUporabnik = bookshelf.Model.extend({
+  tableName:'skupina_uporabnik',  
+   uporabniki: function(){
+    return this.belongsToMany(Uporabnik);
+    }, 
+    skupine: function(){
+      return this.belongsToMany(Skupina);
+    }
+});
 
 
 router.post('/', function(req, res, next) {
 
   var podatki = req.body;
-
+  var idUpo;
   console.log("Podatki: " + JSON.stringify(podatki));
+  //var skupinaUporabnik;
+
+
   var uporabnik = new Uporabnik().where({'up_ime' : podatki.up_ime}).fetch().then(function(rezultat){
 
     if(rezultat !== null && rezultat !== undefined){
       res.json(rezultat.toJSON());
     }
     else{
-      new Uporabnik().save({'up_ime' : podatki.up_ime, 'geslo' : podatki.geslo, 'email' : podatki.email});
-    
+      new Uporabnik().save({'up_ime' : podatki.up_ime, 'geslo' : podatki.geslo, 'email' : podatki.email}).then(function(m) {
+        /*new Uporabnik().where({'up_ime' : podatki.up_ime}).fetch().then(function(rezultat){
+        idUpo=rezultat.toJSON().id;
+        skupinaUporabnik.save({'uporabnik_id': idUpo,'skupina_id':999}).then(function(m) {});
+    });*/
+      new SkupinaUporabnik().save({'uporabnik_id': m.toJSON().id,'skupina_id':999}).then(function(m) {});
+      console.log(m.toJSON());
+
+      });
+      
       res.json(null);
     }
+    
 
   });
     
